bb2a0069d08376dfc20626c36aee43cd733de400
Rahul Kumar <rahulsinner@gmail.com>
2012-05-24 02:48:04 +0530

    First commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+### Django base project.
+
+Base django project based on https://github.com/amccloud/django-project-skel/ and
+https://github.com/xenith/django-base-template
+
+This project isn't very useful.
---
+++ django_project/settings/local.py
@@ -0,0 +1,7 @@
+# Local settings for django_project.
+DEBUG = True
+TEMPLATE_DEBUG = True
+DEV = True
+
+# Show emails in the console during developement.
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
